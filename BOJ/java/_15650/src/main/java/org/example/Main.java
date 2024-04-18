c53e4f43a407e0680882f06e2c2290b4180d2511
ajroot5685 <anjeong8577@naver.com>
2024-04-18 17:07:19 +0900

    15650 - N과 M(2)

--- BOJ/java/_15650/src/main/java/org/example/Main.java
+++ BOJ/java/_15650/src/main/java/org/example/Main.java
@@ -1,7 +1,80 @@
 package org.example;
 
+import java.io.*;
+import java.util.*;
+
 public class Main {
+
+    static FastReader scan = new FastReader();
+    static StringBuilder sb = new StringBuilder();
+
+    static int n;
+    static int m;
+
+    static int[] output;
+    static boolean[] visited;
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        input();
+        solve();
+    }
+
+    static void input() {
+        n = scan.nextInt();
+        m = scan.nextInt();
+    }
+
+    static void solve() {
+        output = new int[m];
+        visited = new boolean[n + 1];
+
+        dfs(0, 0);
+
+        System.out.println(sb);
+    }
+
+    static void dfs(int depth, int index) {
+        if (depth == m) {
+            for (int i = 0; i < m; i++) {
+                sb.append(output[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        for (int i = index + 1; i <= n; i++) {
+            if (visited[i]) {
+                continue;
+            }
+            output[depth] = i;
+            visited[i] = true;
+
+            dfs(depth + 1, i);
+
+            visited[i] = false;
+        }
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
     }
 }
