eacb047b13a200a79f1a5c3cf9d6c09e4270e23d
ajroot5685 <anjeong8577@naver.com>
2025-05-16 11:38:13 +0900

    9251

---
+++ src/main/java/com/baekjoon/lcs/_9251_gold_5.java
@@ -0,0 +1,62 @@
+package com.baekjoon.lcs;
+
+import java.io.*;
+import java.util.*;
+
+public class _9251_gold_5 {
+
+    static FastReader scan = new FastReader();
+
+    static String s1;
+    static String s2;
+
+    public static void main(String[] args) {
+        input();
+        solve();
+    }
+
+    static void input() {
+        s1 = scan.next();
+        s2 = scan.next();
+    }
+
+    static void solve() {
+        int[][] dp = new int[s1.length() + 1][s2.length() + 1];
+
+        for (int i = 1; i <= s1.length(); i++) {
+            for (int j = 1; j <= s2.length(); j++) {
+                if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        System.out.println(dp[s1.length()][s2.length()]);
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+    }
+}
